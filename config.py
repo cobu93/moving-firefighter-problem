f5f586f3a4ecdb58f330f29985033275e1c8e12f
cobu93 <urielcoro@outlook.com>
2023-11-09 13:10:27 -0600

    Adding final configuration for first experiments

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-N_NODES = [15]#[25, 50, 75, 100]
-N_DEGREES = list(range(2, 3))
-N_SAMPLES = 1
+N_NODES = [25, 50, 75, 100]
+N_DEGREES = [5]
+N_SAMPLES = 10
 
 RESULTS_DIR = "results"
 
 RESULTS_FILE = "results.json"
 EXPERIMENTS_FILE = "experiments.json"
 
-RUNNER_TIMEOUT_SEC = 10
+# 24 hours
+RUNNER_TIMEOUT_SEC = 86400
