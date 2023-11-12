3474b33ad9ddbaaa72b0a9be0738503a7fc9ebaa
cobu93 <urielcoro@outlook.com>
2023-11-12 10:57:39 -0600

    Renaming configurations and experiments files

--- config.py
+++ config.py
@@ -1,18 +1,16 @@
 # First set of experiments
 N_NODES = [25, 50, 75, 100]
 ROOT_DEGREE = 3
+RESULTS_FILE = "results_moving_nodes.json"
+EXPERIMENTS_FILE = "experiments_moving_nodes.json"
 
 # Second set of experiments
 # N_NODES = [50]
 # ROOT_DEGREE = [3, 4, 5]
-
+# RESULTS_FILE = "results_moving_root.json"
+# EXPERIMENTS_FILE = "experiments_moving_root.json"
 
 N_SAMPLES = 10
-
 RESULTS_DIR = "results"
-
-RESULTS_FILE = "results.json"
-EXPERIMENTS_FILE = "experiments.json"
-
 # 24 hours
 RUNNER_TIMEOUT_SEC = 86400
