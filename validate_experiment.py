5f1bf0094c664ec535296a169e29d86f8a95c7de
cobu93 <urielcoro@outlook.com>
2023-11-13 15:59:17 -0600

    Script for validating results

--- validate_experiment.py
+++ validate_experiment.py
@@ -4,8 +4,7 @@ import numpy as np
 from config import RESULTS_DIR, EXPERIMENTS_FILE, RESULTS_FILE
 from generators import tree_from_sequence
 
-experiment_id = 2
-
+experiment_id = 1
 experiments_file = os.path.join(RESULTS_DIR, EXPERIMENTS_FILE)
 
 if not os.path.exists(experiments_file):
@@ -40,48 +39,60 @@ for m in results:
             break
 
 
-print(experiment)
-print(experiment_results)
-
 tree = tree_from_sequence(np.array(experiment["sequence"]), add_positions=False)
 tree.nodes_positions = np.array(np.concatenate((
                     experiment["nodes_positions"], 
                     [experiment["initial_firefighter_position"]]
                 ), axis=0))
-tree, _ = tree.to_directed(experiment["root"])
-propagation_time = experiment["propagation_time"]
-path_length = np.argwhere(tree.edges.sum(axis=-1) == 0).shape[0] + 1
 
-print(path_length)
+root = experiment["root"]
+n_nodes = experiment["n_nodes"]
+
+tree, _ = tree.to_directed(root)
+propagation_time = experiment["propagation_time"]
+path_length = np.argwhere(tree.edges.sum(axis=-1) == 0).shape[0]
+
+print("Max path length:", path_length)
 
 for m in experiment_results:
-    print(f"============= {m}")
+    print(f"============================== {m}")
     path = experiment_results[m]["solution"]
+    forest = np.ones(n_nodes).astype(bool)
+    forest[root] = False
     cum_dist = 0
     last_dist = 0
+    saved_nodes = []
+    saving_times = []
 
-    print(path)
+    print("Path:", path)
+    
+    for ff_t in range(path_length):
 
-    t = 0
-    for _ in range(len(path) - 1):
-        current_time = (t + 1) * propagation_time
-        print(f"........ Propagation {t}")
+        if ff_t + 1 > len(path) - 1:
+            print("Left movements: {}".format(path_length - ff_t))
+            break
 
-        cum_dist = last_dist
+        path_to_root = tree.get_path_to_root(path[ff_t + 1])
 
-        while cum_dist < current_time:
+        time_limit = path_to_root.shape[0] * propagation_time
+        cum_dist += np.linalg.norm(tree.nodes_positions[path[ff_t]] - tree.nodes_positions[path[ff_t + 1]])
 
-            if(t > len(path) - 2):
-                break
+        in_forest = forest[path[ff_t + 1]]
+        on_time = cum_dist < time_limit
 
-            dist = np.linalg.norm(tree.nodes_positions[path[t]] - tree.nodes_positions[path[t + 1]])
-            cum_dist += dist
+        valid = in_forest and on_time
 
-            if cum_dist < current_time:
-                print(f"Distance from {path[t]} to {path[t+1]} id {dist}. Total distance: {cum_dist}")
-                t += 1
-            else:
-                last_dist = cum_dist - dist
-                break
-            
+        subtree_nodes = tree.get_subtree_nodes(path[ff_t + 1])
+        saved_nodes.append(len(subtree_nodes))
+        saving_times.append(cum_dist)
+
+        unavailable_nodes = path_to_root.tolist() + subtree_nodes.tolist()
+        forest[unavailable_nodes] = False
+
+        print(path[ff_t], "--", "✓" if valid else "x", "-->", path[ff_t + 1])
+
+    print("Saving times:", saving_times)
+    print("Saved nodes:", saved_nodes)
+    print("Total saved nodes:", np.sum(saved_nodes))
+    
 
