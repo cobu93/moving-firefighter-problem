9e11b617bd92e07788476d03cbfa63737baf5bb6
uriel <uriel@dlworker.rycd.edu>
2024-01-10 10:22:08 -0600

    Updating executions

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # First set of experiments
-N_NODES = [20, 25, 40, 50, 75, 100]
-ROOT_DEGREE = 3
-RESULTS_FILE = "results_moving_nodes.json"
-EXPERIMENTS_FILE = "experiments_moving_nodes.json"
+# N_NODES = [20, 25, 40, 50, 75, 100]
+# ROOT_DEGREE = 3
+# RESULTS_FILE = "results_moving_nodes.json"
+# EXPERIMENTS_FILE = "experiments_moving_nodes.json"
 
 # Second set of experiments
-# N_NODES = [50]
-# ROOT_DEGREE = [3, 4, 5]
-# RESULTS_FILE = "results_moving_root.json"
-# EXPERIMENTS_FILE = "experiments_moving_root.json"
+N_NODES = [40]
+ROOT_DEGREE = [3, 4, 5, 6, 7]
+RESULTS_FILE = "results_moving_roots.json"
+EXPERIMENTS_FILE = "experiments_moving_roots.json"
 
 N_SAMPLES = 10
 RESULTS_DIR = "results"
