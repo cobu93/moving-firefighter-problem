82aec2196a6fa8b537a31e9861f92474cc8c8b68
cobu93 <urielcoro@outlook.com>
2025-09-01 22:03:50 -0600

    Removing MIPGap=0 configuration

--- eilp/runners.py
+++ eilp/runners.py
@@ -88,12 +88,8 @@ class EILP:
 
 
         with gp.Env() as env, gp.Model(env=env) as m:
-            m.setParam("MIPGap", 0)
             m.setParam("NodefileStart", 25)
-            # m.setParam("Presolve", 2)
-            # m.setParam("PreSparsify", 1)
             
-
             self.__setup_problem__(tree, root, t_propagation, m)
 
             def handle_sigterm(signum, frame):
